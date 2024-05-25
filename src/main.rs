b35ba7689fa1b4bc319d645fd7a05f3abae0f216
luispdm <17044119+luispdm@users.noreply.github.com>
2024-05-25 17:30:46 +0800

    add vrf proof and change hash to String (input is now the result of string concatenation)

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ use std::hash::{Hash, Hasher};
 struct Player {
     keypair: Keypair,
     number: u64,
-    number_hash: u64,
+    number_hash: String,
     vrf_output: Option<VRFPreOut>,
-    vrf_proof: Option<VRFProof>
+    vrf_proof: Option<VRFProof>,
 }
 
 impl Player {
@@ -18,7 +18,7 @@ impl Player {
         let number = rand::random::<u64>();
         let mut hasher = DefaultHasher::new();
         number.hash(&mut hasher);
-        let number_hash = hasher.finish();
+        let number_hash = hex::encode(hasher.finish().to_be_bytes());
         Self {
             keypair,
             number,
@@ -44,7 +44,10 @@ impl Player {
         let context = SigningContext::new(b"VRF Poker");
         let context = context.bytes(input);
         if let Some(preout) = &self.vrf_output {
-            self.keypair.public.vrf_verify(context, preout, self.vrf_proof.as_ref().unwrap()).is_ok()
+            self.keypair
+                .public
+                .vrf_verify(context, preout, self.vrf_proof.as_ref().unwrap())
+                .is_ok()
         } else {
             false
         }
@@ -57,21 +60,21 @@ impl Croupier {
     fn verify_hash(player: &Player) -> bool {
         let mut hasher = DefaultHasher::new();
         player.number.hash(&mut hasher);
-        player.number_hash == hasher.finish()
+        hex::encode(hasher.finish().to_be_bytes()) == player.number_hash
     }
 
-    fn collect_inputs(players: &[Player]) -> Vec<u64> {
+    fn collect_hashes(players: &[Player]) -> Vec<String> {
         players
             .iter()
             .filter(|p| Self::verify_hash(p))
-            .map(|p| p.number)
+            .map(|p| p.number_hash.clone())
             .collect()
     }
 
-    fn distribute_input(players: &mut [Player], input: u64) {
-        let input_bytes = input.to_be_bytes();
+    fn distribute_input(players: &mut [Player], input: &String) {
+        let input_bytes = input.as_bytes();
         for player in players.iter_mut() {
-            player.commit(&input_bytes);
+            player.commit(input_bytes);
         }
     }
 }
@@ -80,21 +83,20 @@ fn main() {
     let mut players = vec![Player::new(), Player::new()];
     for player in &players {
         println!(
-            "Player's number: {}, hash: {}",
+            "Player's random input: {}, hash: {}",
             player.number, player.number_hash
         );
     }
 
-    let valid_numbers = Croupier::collect_inputs(&players);
-    let input_sum: u64 = valid_numbers.iter().sum();
+    let final_input = Croupier::collect_hashes(&players).concat();
 
-    Croupier::distribute_input(&mut players, input_sum);
+    Croupier::distribute_input(&mut players, &final_input);
 
     let mut highest_vrf_output: Option<u64> = None;
     let mut winner: Option<usize> = None;
 
     for (i, player) in players.iter().enumerate() {
-        if player.vrf_verify(&input_sum.to_be_bytes()) {
+        if player.vrf_verify(final_input.as_bytes()) {
             if let Some(vrf_output) = player.reveal_vrf_output() {
                 let card_value = (vrf_output.to_bytes()[0] as u64) % 52;
                 println!("Player {}'s card value: {}", i, card_value);
